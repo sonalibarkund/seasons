0f3aced7beb984b37962e48260210a9724797044
sonali-amura <sonali.barkund@sell.do>
2020-03-07 18:09:09 +0530

    season app initialized

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SeasonDisplay = (props) => {
+
+  return(
+    <div>This is summer</div>
+  );
+};
+
+export default SeasonDisplay
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    )
+    return <div> Latitude: </div>;
+  };
+};
+
+ReactDom.render(<App />, document.querySelector("#root"));
